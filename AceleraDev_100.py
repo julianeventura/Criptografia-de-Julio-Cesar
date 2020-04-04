a71aaaf1a18b4d644896c305d1378caaf2a18784
julianeventura <61052573+julianeventura@users.noreply.github.com>
2020-04-03 23:40:37 -0300

    Update AceleraDev_100.py

--- AceleraDev_100.py
+++ AceleraDev_100.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 ###IMPORT DAS BIBLIOTECAS QUE SERÃO USADAS
 
@@ -10,25 +8,13 @@ import requests
 import json
 import hashlib
 
-
-# In[2]:
-
-
 ###REQUISIÇÃO HTTP
 
 response = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=a4299254b61dec7ac5bf17b4c7f0fba487b5624e')
 
-
-# In[3]:
-
-
 ###VERIFICAÇÃO DO QUE TEM DENTRO DA REQUISIÇÃO
 
 response.content
-
-
-# In[4]:
-
 
 ###SALVANDO O ARQUIVO OBTIDO
 
@@ -37,18 +23,10 @@ a = response.json()
 with open('resp_text.json', 'w') as jsonFile:
     json.dump(a, jsonFile)
 
-
-# In[5]:
-
-
 ###CARREGAMENTO DO ARQUIVO
 
 with open('resp_text.json', 'r') as jsonFile:
     answer = json.load(jsonFile)
-
-
-# In[6]:
-
 
 ###CÓDIGO PARA OBTER O TEXTO DECIFRADO
 
@@ -72,83 +50,44 @@ class Caesar:
                 plain_text += idx    
         return plain_text.lower()
 
-
-# In[7]:
-
-
 ###OBTENÇÃO DO TEXTO DECIFRADO
 
 decrypt = Caesar().decrypt(a['cifrado'])
 print(decrypt)
 
-
-# In[8]:
-
-
 ###ATUALIZAÇÃO DO JSON
 
 answer['decifrado'] = decrypt
-
-
-# In[9]:
-
 
 ###CONFERINDO A ATUALIZAÇÃO
 
 answer
 
-
-# In[10]:
-
-
 ###CÓDIGO PARA OBTENÇÃO DO RESUMO ATRAVÉS DO SHA1
 
 hash_object = hashlib.sha1(b'plan to throw one (implementation) away; you will, anyhow. fred brooks')
-
-
-# In[11]:
-
 
 ###OBTENÇÃO DO RESUMO
 
 resumo = hash_object.hexdigest()
 print(hash_object.hexdigest())
 
-
-# In[12]:
-
-
 ###ATUALIZAÇÃO DO JSON
 
 answer['resumo_criptografico'] = resumo
 
-
-# In[13]:
-
-
 ###CONFERINDO A ATUALIZAÇÃO
 
 answer
-
-
-# In[14]:
-
 
 ###SALVANDO O CONTEÚDO FINAL EM UM JSON
 
 with open('answer.json', 'w') as jsonFile:
     json.dump(answer, jsonFile)
 
-
-# In[15]:
-
-
 ###URL DA API PARA A SUBMISSÃO DO ARQUIVO ATUALIZADO
 
 url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token='+answer['token']
-
-
-# In[16]:
 
 
 ###SETANDO OS PARAMETROS EM FILES
@@ -160,17 +99,9 @@ files = {
 
 ###OBS: BOUNDARY OBTIDO ATRAVÉS DO PROGRAMA POSTMAN
 
-
-# In[17]:
-
-
 ###SUBMETENDO O ARQUIVO ATUALIZADO VIA POST PARA A API
 
 r = requests.post(url, files=files)
-
-
-# In[18]:
-
 
 ###RESULTADO DA SUBMISSÃO
 
